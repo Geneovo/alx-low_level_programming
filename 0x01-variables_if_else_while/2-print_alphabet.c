9583e86c82fbc017363bede5d9e147df5998d6b3
Geneovo <bodjuvwederhie@gmail.com>
2023-09-08 14:59:02 -0700

    “2-print_alphabet.c”

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This prints the alphabet in lowercase, followed by a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	    char i = 'a';
+
+	        while (i <= 'z')
+			    {
+				            putchar(i);
+					            i++;
+						        }
+
+		    putchar('\n');
+
+		        return (0);
+}
