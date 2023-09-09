39af2e8d8d985d61d574eb9585218b1f68e3943b
Geneovo <bodjuvwederhie@gmail.com>
2023-09-08 18:29:09 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		for (ones1 = 0; ones1 <= 9; ones1++)
 		{
-			for (ten2 = tens1; tens2 <= 9; tens2++)
+			for (tens2 = tens1; tens2 <= 9; tens2++)
 			{
 				for (ones2 = (tens1 == tens2) ? (ones1 + 1) : 0; ones2 <= 9; ones2++)
 				{
