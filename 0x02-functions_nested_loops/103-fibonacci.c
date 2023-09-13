8585142fab4db69f3e5236bb9dc3b7500a1fa68e
Geneovo <bodjuvwederhie@gmail.com>
2023-09-13 16:10:46 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Each new term in the Fibonacci sequence is generated
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int sum = 0;
+	long int first = 1;
+	long int second = 2;
+
+	while (second <= 4000000)
+	{
+		if (second % 2 == 0)
+			sum += second;
+		long int next = first + second;
+	first = second;
+	second = next;
+	}
+	printf("%ld\n", sum)
+	return (0);
+}
