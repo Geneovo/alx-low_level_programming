191f79f9f7c4d8754e235257ef211ddfbf446458
Geneovo <bodjuvwederhie@gmail.com>
2023-09-14 16:16:17 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ *
+ * @c: input parameter
+ * Return: (1) for true (0) false
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
