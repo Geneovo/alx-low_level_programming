6ec8cb3f0fa52660e04e972fde83315bc677aaaf
Geneovo <bodjuvwederhie@gmail.com>
2023-09-15 15:56:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int pr;
+
+	for (pr = 48; pr <= 57; pr++)
+	{
+		_putchar(pr);
+	}
+
+	_putchar('\n');
+}
