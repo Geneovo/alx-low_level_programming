dd93b799b6a41c9081e162a6b5ca3cdd35cd69d5
Geneovo <bodjuvwederhie@gmail.com>
2023-09-15 17:36:46 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 1; i < 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5 == 0))
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -26,11 +26,10 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
 	}
 	printf("Buzz");
-
 	printf("\n");
 	return (0);
 }
