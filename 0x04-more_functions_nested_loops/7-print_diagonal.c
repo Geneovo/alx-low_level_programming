32e2800b585750fcb210df842408748871e1c84d
Geneovo <bodjuvwederhie@gmail.com>
2023-09-15 18:20:52 -0700

    prints diaonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,13 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; n++)
+	for (i = 0; i < n; i++)
 	{
 		if (n > 1)
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
-
 		_putchar('\\');
 		_putchar('\n');
 	}
