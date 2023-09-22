27ee4a09076eacac8938634013547d19b8bec362
Geneovo <bodjuvwederhie@gmail.com>
2023-09-22 16:11:42 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,8 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-*(p + 5) = 98;
+int *a2_ptr = &a[2];
+*a2_ptr = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
