587e067ebc76f65b4889a4e55a27331de3a4fa87
Geneovo <bodjuvwederhie@gmail.com>
2023-10-10 15:58:02 -0700

    struct that creates a dog info

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Alx structures, typedef task
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that create a dog info
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: first struct with alx
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
