b941080e052bfbfb75d6f888a29a335f6d27c5c6
Geneovo <bodjuvwederhie@gmail.com>
2023-10-20 10:51:25 -0700

    function that prints text

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,12 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - to print a string
+ * first - prints a string
  * Return: nothing
  */
 
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n);"
+	printf("I bore my house upon my back!\n");
 }
