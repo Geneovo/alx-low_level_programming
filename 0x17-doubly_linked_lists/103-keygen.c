b539139804f8be48f8194750d9da07d35331ebbf
Geneovo <bodjuvwederhie@gmail.com>
2023-12-07 17:19:21 -0800

    doubly linked list

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - main function
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	char pwd[7], *d;
+	int x = strlen(argv[1]), y, z;
+
+	d = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+	z = (x ^ 59) & 63;
+	pwd[0] = d[z];
+
+	z = 0;
+	for (y = 0; y < x; y++)
+		z += argv[1][y];
+	pwd[1] = d[(z ^ 79) & 63];
+
+	z = 1;
+	for (y = 0; y < x; y++)
+		z *= argv[1][y];
+	pwd[2] = d[(z ^ 85) & 63];
+
+	z = 0;
+	for (y = 0; y < x; y++)
+	{
+		if (argv[1][y] > z)
+			z = argv[1][y];
+	}
+	srand(z ^ 14);
+	pwd[3] = d[rand() & 63];
+
+	z = 0;
+	for (y = 0; y < x; y++)
+		x += (argv[1][y] * argv[1][y]);
+	pwd[4] = d[(z ^ 239) & 65];
+
+	pwd[6] = '\0';
+	printf("%s", pwd);
+	return (0);
+}
