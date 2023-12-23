3fad854d21a77143a9f012bae53996631a131962
Geneovo <bodjuvwederhie@gmail.com>
2023-12-22 19:48:02 -0800

    Hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - This function deletes a hash table
+ *
+ * @ht: Hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
